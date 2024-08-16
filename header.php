911782d7d96980f50da552e83d8882621ea94e9b
MIGUEL ALEXANDER LOYOLA TORRES <mialoyto1999@gmail.com>
2024-08-16 00:46:19 -0500

    update header.php

--- header.php
+++ header.php
@@ -29,7 +29,7 @@ $host = "http://localhost/kardex";
 
     <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
         <!-- Navbar Brand-->
-        <a class="navbar-brand ps-3" href="index.html">KARDEX</a>
+        <a class="navbar-brand ps-3" href="index.html">KARDEX - Area de developer</a>
         <!-- Sidebar Toggle-->
         <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!">
             <i class="fas fa-bars"></i>
